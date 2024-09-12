078c1ffa215b3333bbec943cf71d80f0377c60e8
aryclemente <hello.aryc@gmail.com>
2024-09-12 01:46:27 -0400

    Merge branch 'main' of https://github.com/aryclemente/laravel_project

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,7 +11,6 @@
 </head>
 
 <body>
-
     <div class="container">
         <div class="nav">
             <ul class="nav justify-content-center nav-tabs">
@@ -43,6 +42,7 @@
         </div>
     </div>
     @yield('contenido')
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
